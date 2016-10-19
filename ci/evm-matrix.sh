5989ac94cae310c3f80845e42834aaf6ddf44611
Ola Nilsson <ola.nilsson@gmail.com>
2016-10-19 20:14:59 +0200

    circleci: Manage evm installs more carefully

--- ci/evm-matrix.sh
+++ ci/evm-matrix.sh
@@ -2,6 +2,7 @@
 #
 total=$2
 index=$1
+errcount=0
 
 errorcounter() {
 	(( errcount++ ))
@@ -13,9 +14,10 @@ trap errorcounter ERR
 evms=$(echo $EVMS | tr ' ' '\n' | awk "NR % $total == $index")
 export EMACS=evm-emacs
 for ever in $evms; do
-	evm install --use emacs-${ever}-travis
+	evm install emacs-${ever}-travis || true
+	evm use emacs-${ever}-travis || exit 2
 	make build
-	mkdir -p ${CIRCLE_TEST_REPORTS:-reports}/$ever
+	[ -d ${CIRCLE_TEST_REPORTS:-reports}/$ever ] || mkdir -p ${CIRCLE_TEST_REPORTS:-reports}/$ever
 	make report JUNIT=${CIRCLE_TEST_REPORTS:-reports}/$ever/junit.xml
 done
 
