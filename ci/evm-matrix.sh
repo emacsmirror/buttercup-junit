5c7c4fc31d22f80683030a4b80340ea1c0f2be5a
Ola Nilsson <ola.nilsson@gmail.com>
2016-10-29 12:02:24 +0200

    circleci: Add tests for present cache dirs

--- ci/evm-matrix.sh
+++ ci/evm-matrix.sh
@@ -10,7 +10,8 @@ errorcounter() {
 
 trap errorcounter ERR
 
-: ${EVMS:="24.2 24.3 24.4 24.5 25.1 git-snapshot"}
+# : ${EVMS:="24.2 24.3 24.4 24.5 25.1 git-snapshot"}
+: ${EVMS:="24.2 24.3 24.4 24.5 25.1"}
 evms=$(echo $EVMS | tr ' ' '\n' | awk "NR % $total == $index")
 export EMACS=evm-emacs
 for ever in $evms; do
