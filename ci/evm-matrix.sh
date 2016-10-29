d3f62482bf9ceaa2bd68d38b06f7162fa645cbd2
Ola Nilsson <ola.nilsson@gmail.com>
2016-10-29 08:51:05 +0200

    circleci: Can't build emacs 24.1 from source, skip for now

--- ci/evm-matrix.sh
+++ ci/evm-matrix.sh
@@ -10,7 +10,7 @@ errorcounter() {
 
 trap errorcounter ERR
 
-: ${EVMS:="24.1 24.2 24.3 24.4 24.5 25.1 git-snapshot"}
+: ${EVMS:="24.2 24.3 24.4 24.5 25.1 git-snapshot"}
 evms=$(echo $EVMS | tr ' ' '\n' | awk "NR % $total == $index")
 export EMACS=evm-emacs
 for ever in $evms; do
