0232a769468cb282044a4420da58494aeeaecfed
Ola Nilsson <ola.nilsson@gmail.com>
2016-10-29 20:06:44 +0200

    circleci: Install prereqs before attempting the initial build

--- ci/prereqs.sh
+++ ci/prereqs.sh
@@ -26,7 +26,7 @@ mkdir -p ~/.evms
 evm config path ~/.evms
 
 # Install one Emacs version, why not the latest release
-evm use emacs-25.1 || evm install --use emacs-25.1
+evm use emacs-25.1 || { install_prereqs; evm install --use emacs-25.1  > emacs-25.1.log ; }
 if [ -d ~/.cask/.git ]; then
 	git -C ~/.cask/ pull
 	#cask upgrade-cask
