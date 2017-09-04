ddd8c513f42298dfd2e5737cd0859515a4ca41c6
Ola Nilsson <ola.nilsson@gmail.com>
2017-09-05 01:31:37 +0200

    circleci: Switch to Circle CI 2.0 and emacs in a docker container

--- ci/ci-matrix.sh
+++ ci/ci-matrix.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-total=$2
-index=$1
+#total=$2
+#index=$1
 errcount=0
 
 errorcounter() {
@@ -10,18 +10,14 @@ errorcounter() {
 
 trap errorcounter ERR
 
-# : ${EVMS:="24.3 24.4 24.5 25.1 git-snapshot"}
-: ${EVMS:="24.3 24.4 24.5 25.1 25.2"}
-evms=$(echo $EVMS | tr ' ' '\n' | awk "NR % $total == $index")
-export EMACS=evm-emacs
-for ever in $evms; do
-	echo Switch to evm emacs $ever
-	evm use emacs-${ever} || exit 2
+: ${EVERS:="24.3 24.4 24.5 25.1 25.2"}
+for ever in $EVERS; do
+	echo Testing GNU Emacs $ever
 	echo "Run 'make build'"
-	make build
+	EMACS=/opt/emacs/$ever/bin/emacs make build
 	reportdir=${CIRCLE_TEST_REPORTS:-reports}/$ever
 	[ -d $reportdir ] || mkdir -p $reportdir
-	export reportfile=$reportdir/junit.xml
+	reportfile=$reportdir/junit.xml
 	echo "Run 'make report', writing report to $reportfile"
 	make report OUTER="Emacs ${ever}" JUNIT=$reportfile
 done
