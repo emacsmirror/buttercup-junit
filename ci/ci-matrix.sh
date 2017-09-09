1db2232737bc89a71173366a2d1e285dfcc2e336
Ola Nilsson <ola.nilsson@gmail.com>
2017-09-10 00:23:15 +0200

    ci: Remove some diagnostic prints from ci-matrix.sh

--- ci/ci-matrix.sh
+++ ci/ci-matrix.sh
@@ -13,12 +13,10 @@ trap errorcounter ERR
 : ${EVERS:="24.3 24.4 24.5 25.1 25.2"}
 for ever in $EVERS; do
 	echo Testing GNU Emacs $ever
-	echo "Run 'make build'"
 	EMACS=/opt/emacs/$ever/bin/emacs make build
 	reportdir=${CIRCLE_TEST_REPORTS:-reports}/$ever
 	[ -d $reportdir ] || mkdir -p $reportdir
 	reportfile=$reportdir/junit.xml
-	echo "Run 'make report', writing report to $reportfile"
 	EMACS=/opt/emacs/$ever/bin/emacs make report OUTER="Emacs ${ever}" JUNIT=$reportfile
 done
 
