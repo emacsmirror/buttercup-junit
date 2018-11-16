d31e45dd4210fb464342e208f8786958c0d3dbc3
Ola Nilsson <ola.nilsson@gmail.com>
2018-11-16 22:21:13 +0100

    Create file test-support
    Extract the testsuites, testsuite, and testcase function (with support
    macros and variables) from test-buttercup-junit.el.
    
    The purpose of this move is to create a subtree to reuse in ert-junit.

--- tests/test-buttercup-junit.el
+++ tests/test-buttercup-junit.el
@@ -30,6 +30,7 @@
 (require 'cl) ; required by esxml
 (require 'esxml)
 (require 'utils-test-buttercup-junit)
+(load "test-support/test-support")
 
 (describe "Calling buttercup-junit-run-discover"
   :var (command-line-args-left buttercup-junit-result-file buttercup-junit--to-stdout)
@@ -195,79 +196,6 @@ will be set to that string value."
 	(expect (buttercup-junit--escape-string "") :to-equal "^A^B^Z"))
   (it "should handle xml entities"
 	(expect (buttercup-junit--escape-string "\"'&") :to-equal "&quot;&apos;&amp;")))
-
-(defun test-buttercup-junit--remove-keys (rest-list &rest keys)
-  "Remove all key-value pairs from REST-LIST for all KEYS.
-Useful to remove &key arguments from a &rest argument in
-`cl-defun's and `cl-defmacro's.
-
-Example:
- (test-buttercup-junit--remove-keys '(1 :foo 2 3) :foo :bar)
- -> '(1 3)"
-  (let (filtered elt)
-    (while rest-list
-      (setq elt (pop rest-list))
-      (if (memq elt keys)
-          (pop rest-list)
-        (push elt filtered)))
-    (nreverse filtered)))
-
-(defun testsuites (&rest contains)
-  "Return an esxml list for a `testsuites' tag.
-CONTAINS should be the contained esxml lists."
-  (declare (indent defun))
-  (if contains
-      `(testsuites nil ,@contains)
-    '(testsuites nil)))
-
-(cl-defun testsuite (name
-                     &rest contains
-                     &key (fail 0) (err 0) (skip 0) (tests (+ fail skip err))
-                     (stamp test-buttercup-junit-timestamp-re)
-                     (host ".+") (time "[0-9]+\\.[0-9]+")
-                     &allow-other-keys)
-  "Return an esxml list for a testsuite tag.
-NAME is the suite description.
-FAIL is the number of failed testcases, default 0.
-ERR is the number of testcases that threw an error, default 0.
-SKIP is the number of skipped (pending) testcases, default 0.
-TESTS is the total number of testcases, defaults to FAIL + ERR + SKIP.
-STAMP should be a JUnit timestamp string or a time value as
-      returned by `current-time'.  STAMP defaults to
-      `test-buttercup-junit-timestamp-re'.
-HOST is a hostname, default `.+'.
-TIME is the elapsed time in seconds, default `[0-9]+\\.[0-9]+'."
-  (declare (indent defun))
-  `(testsuite
-    ((name . ,name)
-     (timestamp . ,(cond ((stringp stamp) stamp)
-                         ((listp stamp)
-                          (regexp-quote
-                           (format-time-string "%Y-%m-%d %T%z" stamp)))
-                         (t (error "Unexpected stamp argument type %s"
-                                   (type-of stamp)))))
-     (hostname . ,host)
-     (tests . ,(number-to-string tests))
-     (failures . ,(number-to-string fail))
-     (errors . ,(number-to-string err)) (time . ,time)
-     (skipped . ,(number-to-string skip)))
-    ,@(test-buttercup-junit--remove-keys contains :fail :err :skip
-                                         :tests :stamp :host :time)))
-
-(cl-defun testcase (name &rest contains &key (class "buttercup") (time "[0-9]+\\.[0-9]+")
-						 skip &allow-other-keys)
-  "Return an esxml list for a testcase tag.
-NAME is the spec description.
-CONTAINS is any inner data for the tag.
-CLASS is the value if the `class' attribute, default `buttercup'.
-TIME is the elapsed time, default `[0-9]+\\.[0-9]+'.
-If SKIP is non-nil, include the `skip' attribute."
-  (declare (indent defun))
-  (let ((attrs `((name . ,name) (classname . ,class) (time . ,time))))
-    (setq contains (test-buttercup-junit--remove-keys contains :class :time))
-	(cond (skip `(testcase ,attrs (skipped nil)))
-		  (contains `(testcase ,attrs ,@contains))
-		  (t `(testcase ,attrs)))))
 
 (describe "JUnit XML output"
   :var ((timestamp (cons 'timestamp test-buttercup-junit-timestamp-re))
